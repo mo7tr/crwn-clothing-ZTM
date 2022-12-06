59ac4f5b75be36d9e344698457fb043db1a83d99
mo7tr <mo7tr@gmail.com>
2022-12-06 16:56:08 +0100

    react router link + navigation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home />} />
+        {/* <Route path="shop" element={<Home />} /> */}
+      </Route>
     </Routes>
   );
 }
