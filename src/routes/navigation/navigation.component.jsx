d9aa9a895ee2e014e5a40aef5722a6c054ce750d
mo7tr <mo7tr@gmail.com>
2022-12-07 15:00:00 +0100

    navigation bar styling

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,16 +1,20 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import "./navigation.styles.scss";
+
 function Navigation() {
   return (
     <Fragment>
-      <div className="Navigation">
-        <Link className="nav-logo-container" to="/">
-          <div>Logo</div>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
-            Shop
+            SHOP
           </Link>
         </div>
       </div>
