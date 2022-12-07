c69336c0d8d23330a671c0c88614256573b793b3
mo7tr <mo7tr@gmail.com>
2022-12-07 15:13:11 +0100

    firebase and sign in page init

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Navigation />}>
         <Route index={true} element={<Home />} />
         {/* <Route path="shop" element={<Home />} /> */}
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
