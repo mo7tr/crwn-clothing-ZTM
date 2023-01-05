1a7999b36be9775fc4a49b477854337501bac08e
mo7tr <mo7tr@gmail.com>
2023-01-05 21:11:38 +0100

    ending up with creating directory-item component and root for category component and preview

--- src/components/Directory/directory.component.jsx
+++ src/components/Directory/directory.component.jsx
@@ -1,11 +1,11 @@
-import CategoryItem from "../category-item/category-item.component";
+import DirectoryItem from "../directory-item/directory-item.component";
 import "./directory.styles.scss";
 
 function Directory({ categories }) {
   return (
     <div className="Directory-container">
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
       ;
     </div>
---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,18 @@
+import "./directory-item.styles.scss";
+
+function DirectoryItem({ category: { imageUrl, title } }) {
+  return (
+    <div className="directory-item-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="body">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+}
+
+export default DirectoryItem;
