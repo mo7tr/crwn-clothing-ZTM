c43d5129342c6047637d998c4e3b7fce9fd6fae1
mo7tr <mo7tr@gmail.com>
2023-01-13 23:16:32 +0100

    user  reducer + category reducer over react + code optimization

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,11 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import { Routes, Route } from "react-router-dom";
+
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 
+import { setCategories } from "../../store/categories/categories.action";
+
 import "./shop.styles.scss";
 
 function Shop() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+
+      dispatch(setCategories(categoriesArray));
+    };
+
+    getCategoriesMap();
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
