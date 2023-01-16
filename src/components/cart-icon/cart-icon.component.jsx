63129450c1bd99ec4a19bf9f3da67fc0d55e68ba
mo7tr <mo7tr@gmail.com>
2023-01-16 15:56:29 +0100

    delete context folder and create cart reducer logic through redux with create selector in order to be more efficient on which component to rerender

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,13 @@
-import { useContext } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 // import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-import { CartContext } from "../../contexts/cart.context";
+import {
+  selectCartCount,
+  selectIsCartOpen,
+} from "../../store/cart/cart.selector.js";
+
+import { setIsCartOpen } from "../../store/cart/cart.action.js";
 
 import {
   CartIconContainer,
@@ -11,9 +16,12 @@ import {
 } from "./cart-icon.styles.jsx";
 
 function CartIcon() {
-  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+  const dispatch = useDispatch();
 
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+  const cartCount = useSelector(selectCartCount);
+  const isCartOpen = useSelector(selectIsCartOpen);
+
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
