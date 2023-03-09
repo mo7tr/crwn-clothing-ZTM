a96605802002e1c9de79231ae709aa1d4ff0a614
mo7tr <mo7tr@gmail.com>
2023-03-09 14:09:29 +0100

    fetching single collection for category component with graphql

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,21 +1,47 @@
 import { useContext, useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 
 import ProductCard from "../../components/product-card/product-card.component";
 import Spinner from "../../components/spinner/spinner.component";
 
-import { CategoriesContext } from "../../contexts/categories.context";
-
 import { CategoryContainer, Title } from "./category.styles";
+
+const GET_CATEGORY = gql`
+  query ($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`;
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  // const { categoriesMap, loading } = useContext(CategoriesContext);
+
+  const { loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category,
+    },
+  });
+
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if (data) {
+      const {
+        getCollectionsByTitle: { items },
+      } = data;
+      setProducts(items);
+    }
+  }, [category, data]);
 
   return (
     <Fragment>
