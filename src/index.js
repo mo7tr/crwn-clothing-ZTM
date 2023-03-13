28bd4f22028b0db4802a1a8beff5b9197ae6948e
mo7tr <mo7tr@gmail.com>
2023-03-13 22:39:47 +0100

    migrating to a PWA

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import App from "./App";
 import { store, persistor } from "./store/store";
 import { stripePromise } from "./utils/stripe/stripe.utils";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 import reportWebVitals from "./reportWebVitals";
 
 import "./index.scss";
@@ -29,6 +31,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
