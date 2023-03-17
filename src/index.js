90c8e781c2836aaf83050869835292e81acedfd0
mo7tr <mo7tr@gmail.com>
2023-03-17 21:19:20 +0100

    mobile and small screen responsiveness

--- src/index.js
+++ src/index.js
@@ -15,8 +15,6 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import reportWebVitals from "./reportWebVitals";
 
-import "./index.scss";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
